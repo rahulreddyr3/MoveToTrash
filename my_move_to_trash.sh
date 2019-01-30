194411056a872e0b8e019fc8a047394910721528
Rahul Reddy <rahul.reddy@go-jek.com>
2019-01-30 18:30:10 +0530

    [Rahul] added support for moving files.

---
+++ my_move_to_trash.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+dest_dir=${2:-"$HOME/.Trash"}
+if [ ! -d $2 ];
+then
+    echo "$2 is not an existing Directory"
+    exit 1;
+fi
+
+if [ -f $1 ]
+then
+    echo "$1 is a file"
+    actual_filename=$1;
+    new_filename="${actual_filename%%.*}";
+    extension="${actual_filename##*.}";
+    if [ -e "$dest_dir/$actual_filename" ]; then
+        increment=1;
+        while [[ -e "$dest_dir/$new_filename-$increment.$extension" ]]; do
+            (( increment++ ))
+        done
+        mv $1 "$dest_dir/$new_filename-$increment.$extension"
+        echo "file $1 moved to trash with name $new_filename-$increment.$extension"
+    else
+        if mv $1 $dest_dir; then
+            echo "file $1 moved to trash with name $actual_filename"
+        else
+            echo "error happened while moving the file $1 to trash"
+        fi
+    fi
+elif [ -d $1 ]
+then
+    echo "$1 is a directory"
+else
+    echo "$1 is neither a File nor Directory"
+    exit 1;
+fi
+exit 0;
