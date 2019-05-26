04f1d06aca514c5c049228c7a130a3ddec18c70f
Rahul <rahulreddyr3@gmail.com>
2019-05-26 23:42:51 +0530

    Can delete files/directories which are not in current directory

--- my_move_to_trash.sh
+++ my_move_to_trash.sh
@@ -3,47 +3,49 @@ dest_dir=${2:-"$HOME/.Trash"}
 if [ ! -d "$dest_dir" ];
 then
     echo "$dest_dir is not an existing Directory"
-    exit 1;
+    exit 1
 fi
 
 if [ -f "$1" ]
 then
-    actual_filename=$1;
-    new_filename="${actual_filename%%.*}";
-    extension="${actual_filename##*.}";
-    if [ -e "$dest_dir/$actual_filename" ]; then
-        increment=1;
-        while [[ -e "$dest_dir/$new_filename-$increment.$extension" ]]; do
+    actual_filename_without_path=${1##*/}
+    echo $actual_filename_without_path
+    actual_filename_without_path_and_extension="${actual_filename_without_path%%.*}"
+    extension="${actual_filename_without_path##*.}"
+    if [ -e "$dest_dir/$actual_filename_without_path" ]; then
+        increment=1
+        while [[ -e "$dest_dir/$actual_filename_without_path_and_extension-$increment.$extension" ]]; do
             (( increment++ ))
         done
-        mv "$1" "$dest_dir/$new_filename-$increment.$extension"
-        echo "file $1 moved to $dest_dir with name $new_filename-$increment.$extension"
+        mv "$1" "$dest_dir/$actual_filename_without_path_and_extension-$increment.$extension"
+        echo "file $1 moved to $dest_dir with name $actual_filename_without_path_and_extension-$increment.$extension"
     else
         if mv "$1" "$dest_dir"; then
-            echo "file $1 moved to $dest_dir with name $actual_filename"
+            echo "file $1 moved to $dest_dir with name $actual_filename_without_path"
         else
             echo "error happened while moving the file $1 to $dest_dir"
         fi
     fi
 elif [ -d "$1" ]
 then
-    actual_directory_name=$1;
-    if [ -d "$dest_dir/$actual_directory_name" ]; then
+    actual_directory_without_path=${1##*/}
+    echo $actual_directory_without_path
+    if [ -d "$dest_dir/$actual_directory_without_path" ]; then
         increment=1;
-        while [[ -e "$dest_dir/$actual_directory_name-$increment" ]]; do
+        while [[ -e "$dest_dir/$actual_directory_without_path-$increment" ]]; do
             (( increment++ ))
         done
-        mv "$1" "$dest_dir/$actual_directory_name-$increment"
-        echo "folder $1 moved to $dest_dir with name $actual_directory_name-$increment"
+        mv "$1" "$dest_dir/$actual_directory_without_path-$increment"
+        echo "folder $1 moved to $dest_dir with name $actual_directory_without_path-$increment"
     else
-        if mv "$1" "$dest_dir/$actual_directory_name" ; then
-            echo "folder $1 moved to $dest_dir with name $actual_directory_name"
+        if mv "$1" "$dest_dir/$actual_directory_without_path" ; then
+            echo "folder $1 moved to $dest_dir with name $actual_directory_without_path"
         else
             echo "error happened while moving the folder $1 to $dest_dir"
         fi
     fi
 else
     echo "$1 is neither a File nor Directory"
-    exit 1;
+    exit 1
 fi
-exit 0;
+exit 0
